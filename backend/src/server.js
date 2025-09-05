4a38325812c24c22377b6e75a6c3d0ecfacf8901
Ananducn <anandu.cn112@gmail.com>
2025-09-05 23:51:51 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { ENV } from "./config/env.js";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(
+    "Server is running on port :",
+    ENV.PORT,
+    `http://localhost:${ENV.PORT}`
+  );
+});
